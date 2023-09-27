d60dcc6b4acd9d8c8ab90794788c741e21128058
hJoyBoyh <rigaudkenny@gmail.com>
2023-09-26 22:56:56 -0400

    loading fini

--- loading/preloader.js
+++ loading/preloader.js
@@ -7,9 +7,36 @@ Pace.on('done', () =>{
     let pre = document.querySelector(".preloader")
     let tl = gsap.timeline();
 
-    tl.to(pre,{
+    tl.to(".bike-ani",{
         duration:2,
+        x:"-30%",
+        ease: Back.easeOut.config(1.7)
+        
+    })
+    tl.add("start")
+    tl.to(".bike-ani",{
+        duration:1.5,
+        x:"150%",
         opacity:0
+        
+        
+    },"start")
+    tl.to(".wind-line-container",{
+        duration:1.5,
+        x:"150%",
+        opacity:0
+        
+        
+    },"start")
+    tl.to(".container",{
+        func:()=>{
+            document.querySelector(".preloader").style.display="none"
+            document.querySelector(".container").style.display="flex"
+        },
+        duration:1.5,
+        
+        opacity:1
+        
         
     })
 
