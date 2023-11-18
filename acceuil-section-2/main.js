731fc5b052a626192268096c81ed47dcea1b57d4
Kenny Rigaud <rigaudkenny@gmail.com>
2023-11-17 23:03:55 -0500

    s

--- acceuil-section-2/main.js
+++ acceuil-section-2/main.js
@@ -1,16 +1,21 @@
 gsap.registerPlugin(ScrollTrigger)
 
 let tl = gsap.timeline()
+tl.to(".bike-gauche",1,{
+    
+    rotationY:"360deg"
+})
 
 
 ScrollTrigger.create({
     trigger:".container-card",
     start: "top top",
-    end: "80% 80%",
+    end: "90% 80%",
    
     toggleActions:"play none none reverse",
     markers:true,
     pin: ".bike-gauche",
+    scrub:true,
    
     animation:tl,
 })
--- siteComplet/js/animation.js
+++ siteComplet/js/animation.js
@@ -10,7 +10,33 @@ tlAnimation.to(".circle",{
 })
 
 // page animation
+//section 1 on the scroll
 
+let section1Animtation = gsap.timeline()
+section1Animtation.add("start")
+
+section1Animtation.to(".bicycle-container",1,{
+    opacity:0,
+    x:500
+},("start"))
+section1Animtation.to(".accueil-info",1,{
+    opacity:0,
+    x:-500
+},("start"))
+
+ScrollTrigger.create({
+    trigger:".section-1",
+    start: "45% 40%",
+   
+    toggleActions:"play none none reverse",
+    markers:true,
+    scrub:true,
+   
+    animation:section1Animtation,
+})
+
+
+//section 2
 let section2TitreAnimation = gsap.timeline()
 
 section2TitreAnimation.from(".section-2-titre",1,{
@@ -20,10 +46,70 @@ section2TitreAnimation.from(".section-2-titre",1,{
 
 ScrollTrigger.create({
     trigger:".section-2-titre",
-    start: "15% 80%",
+    start: "25% 80%",
    
     toggleActions:"play none none reverse",
     markers:true,
+    scrub:true,
    
     animation:section2TitreAnimation,
+})
+
+// content section 2
+let section2ContentAnimation = gsap.timeline()
+section2ContentAnimation.to(".bike-gauche",1,{
+    
+    rotationY:"360deg"
+})
+
+
+ScrollTrigger.create({
+    trigger:".container-card",
+    start: "top top",
+    end: "90% 80%",
+   
+    toggleActions:"play none none reverse",
+    markers:true,
+    pin: ".bike-gauche",
+    scrub:true,
+   
+    animation:section2ContentAnimation,
+})
+//section 3 titre
+let section3TitreAnimation = gsap.timeline()
+
+section3TitreAnimation.from(".section-3-titre",1,{
+    opacity: 0,
+    x:350
+})
+
+ScrollTrigger.create({
+    trigger:".section-3-titre",
+    start: "5% 95%",
+   
+    toggleActions:"play none none reverse",
+    markers:true,
+    scrub:true,
+   
+    animation:section3TitreAnimation,
+})
+
+//section 3
+let section3Animation = gsap.timeline()
+
+section3Animation.from(".services-item",1,{
+    opacity: 0,
+    y:-350,
+    stagger:0.5
+})
+
+ScrollTrigger.create({
+    trigger:".section-3",
+    start: "30% 85%",
+   
+    toggleActions:"play none none reverse",
+    markers:true,
+    
+   
+    animation:section3Animation,
 })
