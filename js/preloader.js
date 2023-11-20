70111c0c6e22c8f33f2bceff6c6aaf5cd6fb5148
javpot <vernetjaven@gmail.com>
2023-11-20 11:05:29 -0500

    preloader test 2

--- js/preloader.js
+++ js/preloader.js
@@ -36,192 +36,192 @@ Pace.on('done', () => {
             document.querySelector(".container").style.display = "block"
 
 
-            if (window.location.pathname == '/' || window.location.pathname == '/index.html') {
-                let introAnimtation = gsap.timeline()
-                introAnimtation.from(".accueil", 2, {
-                    opacity: 0,
-                    x: 500
-                }, ("start"))
+
+            let introAnimtation = gsap.timeline()
+            introAnimtation.from(".accueil", 2, {
+                opacity: 0,
+                x: 500
+            }, ("start"))
 
 
-                let section1Animtation = gsap.timeline()
-                section1Animtation.add("start")
+            let section1Animtation = gsap.timeline()
+            section1Animtation.add("start")
 
-                section1Animtation.to(".bicycle-container", 1, {
-                    opacity: 0,
-                    x: 500
-                }, ("start"))
-                section1Animtation.to(".accueil-info", 1, {
-                    opacity: 0,
-                    x: -500
-                }, ("start"))
-                section1Animtation.to(".scroll-container", 1, {
-                    opacity: 0,
-                    x: -500
-                }, ("start"))
+            section1Animtation.to(".bicycle-container", 1, {
+                opacity: 0,
+                x: 500
+            }, ("start"))
+            section1Animtation.to(".accueil-info", 1, {
+                opacity: 0,
+                x: -500
+            }, ("start"))
+            section1Animtation.to(".scroll-container", 1, {
+                opacity: 0,
+                x: -500
+            }, ("start"))
 
-                ScrollTrigger.create({
-                    trigger: ".section-1",
-                    start: "55% 30%",
-                    end: "100% 10%",
+            ScrollTrigger.create({
+                trigger: ".section-1",
+                start: "55% 30%",
+                end: "100% 10%",
 
-                    // toggleActions:"play none none reverse",
-                    markers: false,
-                    scrub: true,
+                // toggleActions:"play none none reverse",
+                markers: false,
+                scrub: true,
 
-                    animation: section1Animtation,
-                })
+                animation: section1Animtation,
+            })
 
-                //section 2
-                let section2TitreAnimation = gsap.timeline()
+            //section 2
+            let section2TitreAnimation = gsap.timeline()
 
-                section2TitreAnimation.from(".section-2-titre", 1, {
-                    opacity: 0,
-                    x: -350
-                })
+            section2TitreAnimation.from(".section-2-titre", 1, {
+                opacity: 0,
+                x: -350
+            })
 
-                ScrollTrigger.create({
-                    trigger: ".section-2-titre",
-                    start: "25% 80%",
-                    end: "bottom 70%",
+            ScrollTrigger.create({
+                trigger: ".section-2-titre",
+                start: "25% 80%",
+                end: "bottom 70%",
 
-                    toggleActions: "play none none reverse",
-                    markers: false,
-                    scrub: true,
+                toggleActions: "play none none reverse",
+                markers: false,
+                scrub: true,
 
-                    animation: section2TitreAnimation,
-                })
+                animation: section2TitreAnimation,
+            })
 
-                // content section 2
-                let section2ContentAnimation = gsap.timeline()
-                section2ContentAnimation.to(".bike-gauche", 1, {
+            // content section 2
+            let section2ContentAnimation = gsap.timeline()
+            section2ContentAnimation.to(".bike-gauche", 1, {
 
-                    rotationY: "360deg"
-                })
-
-
-                ScrollTrigger.create({
-                    trigger: ".container-card",
-                    start: "top top",
-                    end: "90% 80%",
-
-                    toggleActions: "play none none reverse",
-                    markers: false,
-                    pin: ".bike-gauche",
-                    scrub: true,
-
-                    animation: section2ContentAnimation,
-                })
-                //section 3 titre
-                let section3TitreAnimation = gsap.timeline()
-
-                section3TitreAnimation.from(".section-3-titre", 1, {
-                    opacity: 0,
-                    x: 350
-                })
-
-                ScrollTrigger.create({
-                    trigger: ".section-3-titre",
-                    start: "5% 95%",
-
-                    toggleActions: "play none none reverse",
-                    markers: false,
-                    scrub: true,
-
-                    animation: section3TitreAnimation,
-                })
-
-                //section 3
-                let section3Animation = gsap.timeline()
-
-                section3Animation.from(".services-item", 1, {
-                    opacity: 0,
-                    y: -350,
-                    stagger: 0.5
-                })
-
-                ScrollTrigger.create({
-                    trigger: ".section-3",
-                    start: "30% 85%",
-
-                    toggleActions: "play none none reverse",
-                    markers: false,
+                rotationY: "360deg"
+            })
 
 
-                    animation: section3Animation,
-                })
+            ScrollTrigger.create({
+                trigger: ".container-card",
+                start: "top top",
+                end: "90% 80%",
 
-                function myFunction(x) {
-                    if (x.matches) { // If media query matches
-                        let section1Animtation = gsap.timeline()
-                        section1Animtation.add("start")
+                toggleActions: "play none none reverse",
+                markers: false,
+                pin: ".bike-gauche",
+                scrub: true,
 
-                        document.querySelector(".bicycle-container").style.display = "none"
-                        section1Animtation.to(".bicycle-container", 1, {
-                            opacity: 0,
-                            x: 500
-                        }, ("start"))
-                        section1Animtation.to(".accueil-info", 1, {
-                            opacity: 0,
-                            x: -500
-                        }, ("start"))
+                animation: section2ContentAnimation,
+            })
+            //section 3 titre
+            let section3TitreAnimation = gsap.timeline()
 
-                        ScrollTrigger.create({
-                            trigger: ".section-1",
-                            start: "31% 30%",
-                            end: "100% 10%",
+            section3TitreAnimation.from(".section-3-titre", 1, {
+                opacity: 0,
+                x: 350
+            })
 
-                            // toggleActions:"play none none reverse",
-                            markers: false,
-                            scrub: true,
+            ScrollTrigger.create({
+                trigger: ".section-3-titre",
+                start: "5% 95%",
 
-                            animation: section1Animtation,
-                        })
+                toggleActions: "play none none reverse",
+                markers: false,
+                scrub: true,
 
-                        //section 2 responsive
-                        let section2ContentAnimation = gsap.timeline()
-                        section2ContentAnimation.add("start")
-                        section2ContentAnimation.to(".bike-gauche", 1, {
+                animation: section3TitreAnimation,
+            })
 
-                            rotationY: "360deg"
-                        }, ("start"))
+            //section 3
+            let section3Animation = gsap.timeline()
 
-                        section2ContentAnimation.from(".card-1", 1, {
+            section3Animation.from(".services-item", 1, {
+                opacity: 0,
+                y: -350,
+                stagger: 0.5
+            })
 
-                            xPercent: 1500,
+            ScrollTrigger.create({
+                trigger: ".section-3",
+                start: "30% 85%",
 
-                        }, ("start"))
-                        section2ContentAnimation.from(".card-2", 1, {
-
-                            xPercent: 1500
-                        }, ("start"))
-                        section2ContentAnimation.from(".card-3", 1, {
-
-                            xPercent: 1500
-                        }, ("start"))
+                toggleActions: "play none none reverse",
+                markers: false,
 
 
-                        ScrollTrigger.create({
-                            trigger: ".middle-container",
-                            start: "10% 45%",
-                            end: "250% 80%",
+                animation: section3Animation,
+            })
 
-                            toggleActions: "play none none reverse",
-                            markers: false,
-                            pin: ".section-2",
-                            scrub: true,
+            function myFunction(x) {
+                if (x.matches) { // If media query matches
+                    let section1Animtation = gsap.timeline()
+                    section1Animtation.add("start")
 
-                            animation: section2ContentAnimation,
-                        })
+                    document.querySelector(".bicycle-container").style.display = "none"
+                    section1Animtation.to(".bicycle-container", 1, {
+                        opacity: 0,
+                        x: 500
+                    }, ("start"))
+                    section1Animtation.to(".accueil-info", 1, {
+                        opacity: 0,
+                        x: -500
+                    }, ("start"))
 
-                    }
+                    ScrollTrigger.create({
+                        trigger: ".section-1",
+                        start: "31% 30%",
+                        end: "100% 10%",
+
+                        // toggleActions:"play none none reverse",
+                        markers: false,
+                        scrub: true,
+
+                        animation: section1Animtation,
+                    })
+
+                    //section 2 responsive
+                    let section2ContentAnimation = gsap.timeline()
+                    section2ContentAnimation.add("start")
+                    section2ContentAnimation.to(".bike-gauche", 1, {
+
+                        rotationY: "360deg"
+                    }, ("start"))
+
+                    section2ContentAnimation.from(".card-1", 1, {
+
+                        xPercent: 1500,
+
+                    }, ("start"))
+                    section2ContentAnimation.from(".card-2", 1, {
+
+                        xPercent: 1500
+                    }, ("start"))
+                    section2ContentAnimation.from(".card-3", 1, {
+
+                        xPercent: 1500
+                    }, ("start"))
+
+
+                    ScrollTrigger.create({
+                        trigger: ".middle-container",
+                        start: "10% 45%",
+                        end: "250% 80%",
+
+                        toggleActions: "play none none reverse",
+                        markers: false,
+                        pin: ".section-2",
+                        scrub: true,
+
+                        animation: section2ContentAnimation,
+                    })
+
                 }
-
-                var x = window.matchMedia("(max-width: 800px)")
-                myFunction(x) // Call listener function at run time
-
-
             }
+
+            var x = window.matchMedia("(max-width: 800px)")
+            myFunction(x) // Call listener function at run time
+
+
+
 
         },
         duration: 1.5,
